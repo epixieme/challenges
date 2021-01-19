3f3b4eb8473e1334db80616c5c29f399624682b5
Kirstie Hayes <kirstiehayes@kirsties-mbp.home>
2021-01-19 15:15:35 +0000

    updated design

--- scrimbachallenges/currencyconvertor/currconvert.js
+++ scrimbachallenges/currencyconvertor/currconvert.js
@@ -1,5 +1,3 @@
-
-// Instructions: // 1. Add the functionality to exchange one currency to another//(you can use a technology of your choice) 🤖// 2. Style the app 🎨 const $$ = (ele) => document.querySelector(ele); const rates = {  GBPGBP: 1.0,  EUROEURO: 1.0,  NZDNZD: 1.0,  GBPEURO: 1.1215,  GBPUSD: 1.3644,  GBPNZD: 1.8988,  NZDGBP: 0.53,  NZDUSD: 0.72,  NZDEURO: 0.59,  EUROGBP: 0.89,  EURONZD: 1.68,  EUROUSD: 1.21, };  const convertor = () => {  let returnRate = $$("#original-currency-unit").value.concat($$("#new-currency-unit").value);   $$("#exchange-rate").value = rates[returnRate] || "";    $$(".exchangeRateText").innerHTML = ` Exchange Rate: 1 ${    $$("#original-currency-unit").value  } =  ${rates[returnRate] || ""} ${$$("#new-currency-unit").value}`;
 // Instructions:
 
 // 1. Add the functionality to exchange one currency to another
@@ -8,7 +6,7 @@
 
 const $$ = (ele) => document.querySelector(ele);
 
-const rates = {
+const rates = { //lookup table
   GBPGBP: 1.0,
   EUROEURO: 1.0,
   NZDNZD: 1.0,
@@ -23,27 +21,25 @@ const rates = {
   EUROUSD: 1.21, 
 };
 
-
 const convertor = () => {
+    //join original currency and new currency
   let returnRate = $$("#original-currency-unit").value.concat($$("#new-currency-unit").value);
-
-  $$("#exchange-rate").value = rates[returnRate] || "";
-  
+  $$("#exchange-rate").value = rates[returnRate] || "";//access rates lookup table and return rate value or nothing - prevents undefined from being displayed when only one currency code is chosen
   $$(".exchangeRateText").innerHTML = ` Exchange Rate: 1 ${
     $$("#original-currency-unit").value
   } =  ${rates[returnRate] || ""} ${$$("#new-currency-unit").value}`;
 };
-convertor();
+
+let displayExchangeRateBanner=convertor();//call function to display Exchangre rate banner text
 
 const calculateAmount = () => {
-  convertor();
-//if orig is empty
  $$("#original-currency-amount").value === ""
-    ? $$("#output-text").innerHTML = "Please Enter Amount To Convert"
-    : ($$("#output-text").innerHTML = "", $$("#exchange-rate").value =$$("#original-currency-amount").value * Math.floor($$("#exchange-rate").value * 100)/100);
+    ? $$("#output-text").innerHTML = "Please Enter Amount To Convert"//if amount is null then display output message 
+    : ($$("#output-text").innerHTML = "", $$("#exchange-rate").value =$$ ("#original-currency-amount").value * Math.floor($$("#exchange-rate").value * 100)/100)//else if amount is not null, output message not displayed and currency amount is converted into new currency
+
 }; 
 
-calculateAmount();
+let outPutMessageText = calculateAmount()// call function to display output message 
 
 $$("#original-currency-unit").addEventListener("change", convertor);
 $$("#new-currency-unit").addEventListener("change", convertor);
